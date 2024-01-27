ccbbe353108cdc923b7c5c82f039afac9263f340
cyrej17 <cyrilrejas17@gmail.com>
2024-01-27 22:32:55 +0800

    changes

--- terraform-cloud-aws-dns/my_main_dir/main.tf
+++ terraform-cloud-aws-dns/my_main_dir/main.tf
@@ -2,7 +2,14 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-module "dns" {
-  source    = "../module/aws_dns_route53"
+# module "dns" {
+#   source    = "../module/aws_dns_route53"
+#   dns_entry = var.dns_entry
+# }
+
+  module "dns2" {
+  source  = "app.terraform.io/cyrej_yt_ops/dns2/aws"
+  version = "1.0.0"
   dns_entry = var.dns_entry
-}
+
+  }
